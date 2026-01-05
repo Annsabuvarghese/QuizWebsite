b9127f2161fcb82a4463dd2067d4fc25bd4bc0a8
Annsabuvarghese <annsabu12@gmail.com>
2026-01-05 10:15:41 +0530

    db delete aaki

--- App/models.py
+++ App/models.py
@@ -1,25 +1,23 @@
 from django.db import models
 
-# Create your models here.
-# class Add_Task(models.Model):
-#     task = models.CharField(max_length=100)
-    
-#     def __str__(self):
-#         return self.task
+class AddCategory(models.Model):
+    name = models.CharField(max_length=100,unique=True,default='python')
 
+    def __str__(self):
+        return self.name
 
 class AddQues(models.Model):
-    Ques = models.CharField(max_length=1000)
+    Ques = models.TextField()
     op1 = models.CharField(max_length=300)
     op2 = models.CharField(max_length=300)
     op3 = models.CharField(max_length=300)
     op4 = models.CharField(max_length=300)
     correct =models.CharField(max_length=300,default='op1')
-    category = models.CharField(max_length=200,default='python')
+    category = models.ForeignKey(AddCategory,on_delete = models.CASCADE)
 
-class UserReg(models.Model):
-    name = models.CharField(max_length=200)
-    age= models.CharField(max_length=3)
-    email =models.EmailField(max_length=300)
-    password = models.CharField(max_length=300)
+# class UserReg(models.Model):
+#     name = models.CharField(max_length=200)
+#     age= models.CharField(max_length=3)
+#     email =models.EmailField(max_length=300)
+#     password = models.CharField(max_length=300)
     
